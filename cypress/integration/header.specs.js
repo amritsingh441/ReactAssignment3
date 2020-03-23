64ccdd42189d1bc83f8fa905eed2e257974646fb
Ubuntu <ubuntu@ip-172-31-32-139.ec2.internal>
2020-03-23 06:47:55 +0000

    cypress test case for header comp

--- cypress/integration/header.specs.js
+++ cypress/integration/header.specs.js
@@ -1,17 +1,23 @@
 describe("UI Testing for header component", () => {
     beforeEach(()=>{
         cy.visit("/");
-        cy.get('input[id=userId]').type('admin');
-        cy.get('input[id=usrPassword]').type('password');
-        cy.get('#loginBtn').click();
-        cy.visit("http://localhost:3000/dashboard");
     })
 
     it("Header dashboard button validation success", () => {
+        cy.get('input[id=userId]').type('admin');
+        cy.get('input[id=usrPassword]').type('password');
+        cy.get('#loginBtn').click();
+        cy.get().should('include','3000/dashboard');
         cy.get('#dashBoardBtn').click();
+        cy.get().should('include','3000/dashboard');
     })
 
     it("Header readNow button validation success", () => {
+        cy.get('input[id=userId]').type('admin');
+        cy.get('input[id=usrPassword]').type('password');
+        cy.get('#loginBtn').click();
+        cy.get().should('include','3000/dashboard');
         cy.get('#readNowBtn').click();
+        cy.get().should('include','3000/readnow');
     })
 })
